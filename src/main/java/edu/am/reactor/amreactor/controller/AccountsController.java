d30a4bdc62444ae9e628d1639bf0af04e771d26a
iamtech <iamtech4k@gmail.com>
2021-05-18 20:15:32 +0530

    AccountsController class added with new methods in AccountsRepository.java

---
+++ src/main/java/edu/am/reactor/amreactor/controller/AccountsController.java
@@ -0,0 +1,41 @@
+package edu.am.reactor.amreactor.controller;
+
+import edu.am.reactor.amreactor.document.AccountsDocument;
+import edu.am.reactor.amreactor.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(path="/accounts")
+public class AccountsController {
+
+    @Autowired
+    AccountsRepository accountsRepository;
+
+    @GetMapping("/get")
+    public Flux<AccountsDocument> getAllAccounts(){
+       return accountsRepository.findAll();
+    }
+
+    @GetMapping("/getLimited/{limit}")
+    public Flux<AccountsDocument> getLimitedAccounts(@PathVariable long limit){
+        return accountsRepository.findAll().limitRequest(limit);
+    }
+
+    @GetMapping("/getById/{id}")
+    public Mono<ResponseEntity<AccountsDocument>> getAccountById(@PathVariable int id){
+        return accountsRepository.findByAccountId(id)
+                .map((accountsData) -> new ResponseEntity<>(accountsData, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    /*    Output:
+        {"id":"5ca4bbc7a2dd94ee5816239a","accountId":370583,"limit":10000,"products":["Brokerage","Commodity","InvestmentStock"]}*/
+    }
+}
--- src/main/java/edu/am/reactor/amreactor/repository/AccountsRepository.java
+++ src/main/java/edu/am/reactor/amreactor/repository/AccountsRepository.java
@@ -3,10 +3,13 @@ package edu.am.reactor.amreactor.repository;
 import edu.am.reactor.amreactor.document.AccountsDocument;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface AccountsRepository extends ReactiveMongoRepository<AccountsDocument,String> {
 
     Flux<AccountsDocument> findByLimit(int limit);
 
     Flux<AccountsDocument> findByProductsContaining(String product);
+
+    Mono<AccountsDocument> findByAccountId(int accountId);
 }
