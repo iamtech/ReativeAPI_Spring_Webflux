1bcb287d3a896ef9796fa95f1ac9789adfb805f8
iamtech <iamtech4k@gmail.com>
2021-05-18 20:14:10 +0530

    Mapped AccountsDocument class to MongoDb Atlas Collection "accounts"

--- src/main/java/edu/am/reactor/amreactor/document/AccountsDocument.java
+++ src/main/java/edu/am/reactor/amreactor/document/AccountsDocument.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
-@Document
+@Document(collection = "accounts")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,7 +18,14 @@ public class AccountsDocument {
 
     @Id
     String id;
+
+    @Indexed(unique = true)
+    @Field(value = "account_id")
     int accountId;
+
+   // @Field(value = "limit")
     int limit;
+
+    // @Field(value = "products")
     List<String> products;
 }
